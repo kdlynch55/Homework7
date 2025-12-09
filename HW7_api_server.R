8f67d9969387b205224a0068ea2138a101d08d66
Kevin Lynch <155801346+kdlynch55@users.noreply.github.com>
2025-12-09 01:40:01 -0600

    Update HW7_api_server.R

--- HW7_api_server.R
+++ HW7_api_server.R
@@ -1,1 +1,29 @@
-## Write your API endpoints in this file
+library(plumber2) 
+library(randomForest) 
+
+model <- readRDS("HW4_No_Show_Model.rds") 
+
+#* Binary No Show Predictions 
+#* @post /predict_class 
+#* @parser rds 
+#* @serializer rds 
+function(req){ 
+  body <- req$body
+  df <- as.data.frame(body) 
+  message(sprintf("Making binary predictions for %d rows of data", nrow(df))) 
+  probs <- predict(model, newdata = df, type = 'prob')[,2] 
+  pred <- as.integer(probs >= 0.5) 
+  return(pred) 
+} 
+
+#* Probability No Show Predictions 
+#* @post /predict_prob 
+#* @parser rds 
+#* @serializer rds 
+function(req){ 
+  body <- req$body
+  df <- as.data.frame(body) 
+  message(sprintf("Making probability predictions for %d rows of data", nrow(df))) 
+  probs <- predict(model, newdata = df, type = 'prob')[,2] 
+  return(probs) 
+} 
